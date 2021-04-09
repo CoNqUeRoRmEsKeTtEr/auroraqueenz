ce3048ca07e061fef7d3a9e79dcb5642559c6be1
dangerbots <79362039+dangerbots@users.noreply.github.com>
2021-04-09 22:28:11 +0530

    Update __telegraph.py

--- Elizabeth/modules/__telegraph.py
+++ Elizabeth/modules/__telegraph.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
 Danger_of_telegram= "Queen"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=darkprince)
+r = telegraph.create_account(short_name=Elizabeth)
 auth_url = r["auth_url"]
 
 
